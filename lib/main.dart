8c3ea46f9773eda68156f69be5a96f971e36bf4c
rrifafauzikomara <rifafauzi6@gmail.com>
2019-05-23 01:05:17 +0700

    add codelab 4 for widget testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-//      initialRoute: '/',
-//      routes: {
-//        // Ketika navigasi ke route "/", buat HomeScreen Widget
-//        '/': (context) => HomeScreen(),
-//        // ketika navigasi ke route "/detail", buat DetailScreen Widget
-//        '/detail': (context) => DetailScreen(),
-//      },
-      title: 'Bahan Makanan',
-      home: HomeScreen()
-    );
-  }
+void main() {
+  runApp(
+    MaterialApp(
+      home: AppHome(),
+    ),
+  );
 }
 
-class HomeScreen extends StatefulWidget {
-  @override
-  HomePageState createState() => HomePageState();
-}
-
-class HomePageState extends State<HomeScreen> {
-
-  List<String> data = ['Chicken','Salmon','Beef','Pork','Avocado','Apple Cider Vinegar','Asparagus'];
-
+class AppHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: const Text('Bahan Makanan'),
-      ),
-      body: ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(data[index]),
-//            onTap: () {
-//              Navigator.pushNamed(context, '/detail', arguments: );
-//            },
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DetailScreen(item: data[index]),
-                  ),
-                );
-              },
-          );
-        },
-      ),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-
-  DetailScreen({Key key, this.item}) : super(key: key);
-  final String item;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text('Detail Bahan Makanan'),
-          leading: IconButton(icon:Icon(Icons.arrow_back),
-            onPressed:() => Navigator.pop(context, false),
-          )
-      ),
-      body: Center(
-        child: Text(item),
+    return Material(
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            debugDumpApp();
+          },
+          child: Text('Dump App'),
+        ),
       ),
     );
   }
