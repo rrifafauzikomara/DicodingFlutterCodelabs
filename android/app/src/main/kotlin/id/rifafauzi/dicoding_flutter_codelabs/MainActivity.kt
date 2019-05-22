668eac564f83f7cea8e6213933b919394850f366
rrifafauzikomara <rifafauzi6@gmail.com>
2019-05-22 19:13:00 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/id/rifafauzi/dicoding_flutter_codelabs/MainActivity.kt
@@ -0,0 +1,13 @@
+package id.rifafauzi.dicoding_flutter_codelabs
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
